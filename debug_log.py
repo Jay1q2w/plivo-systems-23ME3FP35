ef8c4fdf02c1eb4ac4a9de69111be8a545d30813
Jay Gheewala <jaygheewala06@gmail.com>
2026-07-15 16:02:09 +0000

    Remove unnecessary debug scripts from tracking

--- debug_log.py
+++
@@ -1,7 +0,0 @@
-import json
-log = json.load(open('playout_log.json'))
-frames = log['frames']
-present = sum(1 for f in frames if f['present'])
-print(f'Present: {present}/{len(frames)}')
-for f in frames[:10]:
-    print(f)
